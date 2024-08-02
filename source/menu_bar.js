374911c104bad37933be91ab9b0e9d389c79c28c
TriniLM <gregoritrinidad110299@gmail.com>
2024-08-02 10:34:52 -0400

    agregando los enlaces a la seccion de marcas y las descripciones de marcas tambien agregamos el texto de best select.

--- source/menu_bar.js
+++ source/menu_bar.js
@@ -1,5 +1,8 @@
-let menuBar = document.getElementById("menu__bar");
-let header = document.getElementById("header");
-menuBar.addEventListener("click",(e)=>{
+window.addEventListener("load", ()=>{
+    let menuBar = document.getElementById("menu__bar");
+    let header = document.getElementById("header");
+    menuBar.addEventListener("click",(e)=>{
     header.classList.toggle("header__menu-bar-active");
+    console.log("menu")
+})
 })
