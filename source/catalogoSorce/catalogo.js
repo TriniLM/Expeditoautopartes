dbc12df35d9358c6431d54905b6f2b5c94c58c9e
TriniLM <gregoritrinidad110299@gmail.com>
2024-08-09 10:32:43 -0400

    seccion de catalogo modificada

--- source/catalogoSorce/catalogo.js
+++ source/catalogoSorce/catalogo.js
@@ -1,5 +1,16 @@
-const navCatalogo = document.getElementById("navCatalogo");
-const navBtnCatalogo = document.getElementById("nav__btn-catalogo");
-navBtnCatalogo.addEventListener("click", ()=>{
-    navCatalogo.classList.toggle("nav__active")
+import functionObserver from "../observerFunction.js";
+functionObserver
+window.addEventListener("load", () => {
+    const cardCategory = document.querySelectorAll(".card-category");
+    const btnCategory = document.querySelectorAll(".card-category__buttom");
+    btnCategory.forEach((e) => {
+        e.addEventListener("click", (e) => {
+            let parameter = e.target.getAttribute('btn-category');
+            window.location.href = `../../page/categoriaList.html?categoria=${parameter}`;
+        })
+    })
+
+    cardCategory.forEach((e)=>{
+        functionObserver(e,"btn-category_visible")
+    })
 })
--- source/catalogoSorce/catalogoService.js
+++ source/catalogoSorce/catalogoService.js
@@ -1,25 +1,24 @@
-/*window.addEventListener("load", async()=>{
-// obeteniendo las categorias donde incrustaremos la lista de productos
-const bandas = document.getElementById("bandas");
-const casquillo = document.getElementById("casquillo");
-const chapa = document.getElementById("chapa");
-const convertidor = document.getElementById("convertidor");
-const discoMetal = document.getElementById("discoMetal");
-//obteniendo el titulo de la categoria para hacer que cuando este se muestre en pantalla la lista se cargue
-const bandasIntersecting = document.getElementById("bandasInterseting");
-const casquilloIntersecting =  document.getElementById("casquilloIntersecting");
-const chapaIntersecting =  document.getElementById("chapaIntersecting");
-const convertidorIntersecting  = document.getElementById("convertidorIntersecting");
-const discoMetalIntersecting =  document.getElementById("discoMetalIntersecting");
 
+/*window.addEventListener("load", async()=>{
+// obeteniendo las categorias
 // haciendo peticion para recibir la lista de productos
-const getData = async () =>{
-    let res = await fetch("https://cdn.jsdelivr.net/gh/TriniLM/dbEspedito@main/catalogo.json");
-    let data = await res.json();
-    return data;
-}
-const product = await getData();
-//funcion para renderizar o dibujar la lista de productos
+
+
+const categoria = new URLSearchParams(window.location.search).get("categoria");
+// aqui vamos a declarar las funciones que se ejecutaran cuando el titulo entre en el viewport
+
+})
+*/
+window.addEventListener("load",async ()=>{
+    const catalogoContent = document.getElementById("productos");
+    const categoria = new URLSearchParams(window.location.search).get('categoria');
+    const getData = async () =>{
+        let res = await fetch("https://cdn.jsdelivr.net/gh/TriniLM/dbEspedito@main/catalogo.json");
+        let data = await res.json();
+        return data;
+    }
+    const product = await getData();
+    //funcion para renderizar o dibujar la lista de productos
 const renderList = (name,description)=>{
     let li = document.createElement("li");
     li.classList.add("content-product__items-product");
@@ -34,40 +33,12 @@ const renderList = (name,description)=>{
     li.innerHTML = template;
     return li;
 }
-//funcion observadora del titulo para cuando este se muestre en el viewport se ejecute la funcion que va 
-//incrustar los elementos
-function functionObserver(element,functionEjecut) {
-    // Definir el callback handleIntersect
-    let handleIntersect = (entries, ) => {
-        if (entries[0].isIntersecting) {
-            functionEjecut()
-            observer.disconnect()
-        }
-    };
-    // Definir las opciones del IntersectionObserver
-    const options = {
-        root: null, 
-        rootMargin: '0px',
-        threshold: 0.1
-    };
-    // Crear el observador
-    let observer = new IntersectionObserver(handleIntersect, options);
-    observer.observe(element);
-}
-// aqui vamos a declarar las funciones que se ejecutaran cuando el titulo entre en el viewport
 const renderProduct = (parent, categoria)=>{
-        product.catalogo.filter(elem => {
-         if(elem.Categoría === categoria){
-             parent.appendChild(renderList(elem.Categoría,elem.Descripción))
-        }
-     })  
+    product.catalogo.filter(elem => {
+     if(elem.Categoría === categoria){
+         parent.appendChild(renderList(elem.Categoría,elem.Descripción))
+    }
+ }) 
 }
-
-// ejecucion del observador dandole el elemento a observar y la funcion a ejecutar
-functionObserver(bandasIntersecting,()=> renderProduct(bandas,"BANDAS"))
-functionObserver(casquilloIntersecting,()=> renderProduct(casquillo,"CASQUILLO"))
-functionObserver(chapaIntersecting,()=> renderProduct(chapa,"CHAPAS"))
-functionObserver(convertidorIntersecting,()=> renderProduct(convertidor,"CONVERTIDOR"));
-functionObserver(discoMetalIntersecting,()=> renderProduct(discoMetal,"DISCO DE METAL"))
-console.log(product)
-})*/
+renderProduct(catalogoContent,categoria)
+ }) 
