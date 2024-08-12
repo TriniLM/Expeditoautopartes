285ff91fa9d681d8404a0eb7b79db0e5c878efde
TriniLM <gregoritrinidad110299@gmail.com>
2024-08-12 09:17:03 -0400

    modificando seccion de catalogo y agregando la seccion para mostrar los productos.

--- source/catalogoSorce/catalogoService.js
+++ source/catalogoSorce/catalogoService.js
@@ -1,14 +1,4 @@
-
-/*window.addEventListener("load", async()=>{
-// obeteniendo las categorias
-// haciendo peticion para recibir la lista de productos
-
-
-const categoria = new URLSearchParams(window.location.search).get("categoria");
-// aqui vamos a declarar las funciones que se ejecutaran cuando el titulo entre en el viewport
-
-})
-*/
+import functionObserver from "../observerFunction.js";
 window.addEventListener("load",async ()=>{
     const catalogoContent = document.getElementById("productos");
     const categoria = new URLSearchParams(window.location.search).get('categoria');
@@ -27,18 +17,29 @@ const renderList = (name,description)=>{
                      </figure>
                     <h3 class="items-product__title">${description}</h3>
                     <a href="https://wa.me/18097052885?text=Hola, me interesaría saber más sobre: ${description} https://trinilm.github.io/Expeditoautopartes/img/product-hot/200872.png" target"_black"> 
-                    <button class="items-product__btn">Pedir</button>
+                    <button class="items-product__btn">Más detalles</button>
                     </a>
                     `;
     li.innerHTML = template;
     return li;
 }
-const renderProduct = (parent, categoria)=>{
-    product.catalogo.filter(elem => {
-     if(elem.Categoría === categoria){
-         parent.appendChild(renderList(elem.Categoría,elem.Descripción))
+const renderProduct =(parent, categoria)=>{
+    if(categoria ==="TODAS"){
+        product.catalogo.forEach(e =>{
+            parent.appendChild(renderList(e.Categoría,e.Descripción))
+        })
     }
- }) 
-}
-renderProduct(catalogoContent,categoria)
+    else{
+        product.catalogo.filter(elem => {
+            if(elem.Categoría === categoria){
+              parent.appendChild(renderList(elem.Categoría,elem.Descripción))
+           }
+    } 
+ ) 
+}}
+   renderProduct(catalogoContent,categoria); 
+   let cardsProduct = document.querySelectorAll(".content-product__items-product");
+   cardsProduct.forEach((e)=>{
+    functionObserver(e, "card-product__active")
+   })
  }) 
---
+++ source/catalogoSorce/menuBar.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",()=>{
+    const btnNavProduct = document.getElementById("btnProductNav");
+    const productsNavlist = document.getElementById("productsNavlist");
+    const itemsProductList = document.querySelectorAll(".products-nav__list-items");
+    btnNavProduct.addEventListener("click", ()=>{
+        productsNavlist.classList.toggle("products-nav__list-active")
+    })
+    itemsProductList.forEach((e)=>{
+        e.addEventListener("click",(e)=>{
+            let newParamenter = e.target.getAttribute("categoria-name");
+            window.location.href = `categoriaList.html?categoria=${newParamenter}`;
+        })
+    })
+})
