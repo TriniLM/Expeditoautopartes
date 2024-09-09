b0194639b9e3ff152ab914cfceb1bb317428d59a
TriniLM <gregoritrinidad110299@gmail.com>
2024-09-09 10:40:41 -0400

    agregando el nuevo logo, agregar animaciones a la seccion de envios y modificando el slider de promociones.

--- source/marcasObserve.js
+++ source/marcasObserve.js
@@ -1,7 +1,6 @@
 import functionObserver from "./observerFunction.js";
 window.addEventListener("load",()=>{
 const marcas =  document.querySelectorAll(".marcas-cards__card");
-console.log(marcas)
 marcas.forEach(element => {
     functionObserver(element, "marcas-cards__card-visible");
 })
