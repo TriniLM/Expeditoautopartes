7160b9859385e7ee40298502f8856690db88d0be
TriniLM <gregoritrinidad110299@gmail.com>
2024-10-09 10:44:02 -0400

    cambios en el catalogo y en en la seccion de productos mas vendidos

--- source/product-hot/productHot.js
+++ source/product-hot/productHot.js
@@ -1,37 +1,21 @@
-const productContent = document.getElementById("produtHotContent");
-const productCard = document.querySelectorAll(".product-hot__card");
-const left = document.getElementById('leftProduct');
-const rigth = document.getElementById("rigthProduct")
-let counter = 0;
-let detener = false;
-rigth.addEventListener("click",()=>{
-  if(!detener){
-    counter++;
-    console.log(counter)
-    let traslateCalc = counter * 300 + 20;
-    productContent.style.transform = `translateX(-${traslateCalc}px)`
-  }
-})
-left.addEventListener("click",(e)=>{
-    if(!counter == 0){
-        console.log(counter)
-        counter--;
-        let traslateCalc = counter * 300 + 40;
-        productContent.style.transform = `translateX(-${traslateCalc}px)`
-      }
-})
-const handleIntersect = (entries,observe)=>{
-if(entries[0].isIntersecting){
-    detener = true;
-}
-else{
-    detener = false;
-}
-}
-let observer = new IntersectionObserver(handleIntersect,{
-    root: null, // Usa el viewport como root
-    rootMargin: '0px', // Sin margen
-    threshold: .2// 10% de visibilidad
+window.addEventListener("load",()=>{
+  const productContent = document.getElementById("produtHotContent");
+  const productCard = document.querySelectorAll(".product-hot__card");
+  const productConteiner = document.querySelector(".product-hot-conteiner");
+  const left = document.getElementById("product-hot_left");
+  const right = document.getElementById("product-hot_right");
+  
+  right.addEventListener("click",()=>{
+    productConteiner.scrollBy({
+      left:300,
+      behavior:"smooth"
+    });
+  });
+  
+  left.addEventListener("click",()=>{
+    productConteiner.scrollBy({
+      left:-300,
+      behavior:"smooth"
+    });
+  });
 });
-
-observer.observe(productCard[9])
